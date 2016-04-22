b61618194b19f6cbf4a91530c3e6ee03103b2223
inphood <prabhaav.bhardwaj@gmail.com>
2016-04-21 23:00:24 -0600

    adding framerwork for fb login

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,10 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-var Camera = require("react-native-camera");
+
+'use strict';
+
+var InPhoodNavigation = require('./InPhoodNavigation');
 
 import React, {
   AppRegistry,
@@ -13,62 +16,16 @@ import React, {
   View
 } from 'react-native';
 
-
-class inPhoodRN extends Component {
-  constructor() {
-    super();
-
-    // From: https://facebook.github.io/react/docs/reusable-components.html#no-autobinding
-    this._switchCamera = this._switchCamera.bind(this);
-    this._takePicture = this._takePicture.bind(this);
-
-    this.state = {cameraType: Camera.constants.Type.back};
-  }
-
-  _switchCamera() {
-    this.state.cameraType = this.state.cameraType === Camera.constants.Type.back ? Camera.constants.Type.front : Camera.constants.Type.back;
-    this.setState(this.state);
-
-    console.log("In _switchCamera:");
-    console.log("%s", this.state.cameraType)
-
-  }
-
-  _takePicture() {
-    this.refs.cam.capture(function(err, data) {
-      console.log(err, data);
-    });
-  }
-
+class InPhoodApp extends React.Component {
   render() {
     return (
-
-      <Camera
-        ref="cam"
+      <React.NavigatorIOS
         style={styles.container}
-        type={this.state.cameraType}>
-        <View style={styles.buttonBar}>
-          <TouchableHighlight style={styles.button} onPress={this._switchCamera}>
-            <Text style={styles.buttonText}>Flip</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={styles.button} onPress={this._takePicture}>
-            <Text style={styles.buttonText}>Take</Text>
-          </TouchableHighlight>
-        </View>
-      </Camera>
-/*
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-         inPhood! 
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>*/
+        initialRoute={{
+          title: 'LandingPage',
+          component: InPhoodNavigation,
+        }}
+      />
     );
   }
 }
@@ -76,45 +33,13 @@ class inPhoodRN extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
   },
-  buttonBar: {
-    flexDirection: "row",
-    position: "absolute",
-    bottom: 25,
-    right: 0,
-    left: 0,
-    justifyContent: "center"
+    text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
   },
-  button: {
-    padding: 10,
-    // color: "#FFFFFF",
-    borderWidth: 1,
-    borderColor: "#FFFFFF",
-    margin: 5
-  },
-  buttonText: {
-    color: "#FFFFFF"
-  }
-
-  /*container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },*/
 });
 
-AppRegistry.registerComponent('inPhoodRN', () => inPhoodRN);
+AppRegistry.registerComponent('inPhoodRN', () => InPhoodApp);
--- ios/inPhoodRN/AppDelegate.m
+++ ios/inPhoodRN/AppDelegate.m
@@ -10,6 +10,8 @@
 #import "AppDelegate.h"
 
 #import "RCTRootView.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @implementation AppDelegate
 
@@ -31,8 +33,8 @@
    * on the same Wi-Fi network.
    */
 
-//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-  jsCodeLocation = [NSURL URLWithString:@"http://10.0.1.8:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  //jsCodeLocation = [NSURL URLWithString:@"http://10.0.1.8:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -54,7 +56,22 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
   return YES;
 }
 
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation];
+}
 @end
